d808c2831f7b643552e0470355aac9865729e8b4
donggun <hugh3232@naver.com>
2026-03-11 15:46:46 +0900

    feat: ios 세팅

---
+++ apps/ios/InVoiceFlow_iOS/InVoiceFlow_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InVoiceFlow_iOS
+//
+//  Created by 하동건 on 3/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apps/ios/InVoiceFlow_iOS/InVoiceFlow_iOS/InVoiceFlow_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InVoiceFlow_iOSApp.swift
+//  InVoiceFlow_iOS
+//
+//  Created by 하동건 on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct InVoiceFlow_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
